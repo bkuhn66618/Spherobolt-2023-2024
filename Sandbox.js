2af095516d9ca5ebe1cee45c277c7a97ffc9c77a
BrianK66618 <kbrian0618@gmail.com>
2024-01-11 08:13:01 -0500

    Hello World connected and stable

--- Sandbox.js
+++ Sandbox.js
@@ -1,3 +1,13 @@
 //Programmer: Brian
 //Date: 1.10.2024
 //Program: Sphero Bolt Testing
+//Bolt ID: SB-F28B
+
+async function startProgram() {
+	await speak("Hello World", true);
+	setMainLed({ r: 145, g: 0, b: 100});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+	
+}
