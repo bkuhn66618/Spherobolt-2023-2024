1b7d645b7f596f3debfd0fe021d506053a601d83
BrianK66618 <kbrian0618@gmail.com>
2024-01-11 08:42:07 -0500

    Figure 8

--- Sandbox.js
+++ Sandbox.js
@@ -2,7 +2,7 @@
 //Date: 1.10.2024
 //Program: Sphero Bolt Testing
 //Bolt ID: SB-F28B
-
+/*
 async function startProgram() {
 	await speak("Hello World", true);
 	setMainLed({ r: 145, g: 0, b: 100});
@@ -11,3 +11,7 @@ async function startProgram() {
 	setSpeed(0);
 	
 }
+*/
+
+# Figure 8
+
