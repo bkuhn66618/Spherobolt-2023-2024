f7294d8611b1476c846d483be35984d7a2bdc747
BrianK66618 <kbrian0618@gmail.com>
2024-01-18 07:53:46 -0500

    Figure 8 + order changes

--- Sandbox.js
+++ Sandbox.js
@@ -2,6 +2,7 @@
 //Date: 1.10.2024
 //Program: Sphero Bolt Testing
 //Bolt ID: SB-F28B
+
 /*
 async function startProgram() {
 	await speak("Hello World", true);
@@ -13,5 +14,22 @@ async function startProgram() {
 }
 */
 
-# Figure 8
+// Square w/ different colors for each line
 
+
+
+
+
+
+// Square w/ different colors & Sound for each line
+
+
+
+
+
+
+// Figure 8
+for (var _i1 = 0; _i1 < 8; _i1++) {
+	await roll(360, 15, 4)
+	await roll(-360, 15, 4)
+}
