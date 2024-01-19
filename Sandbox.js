62fff67b070752a2ba916d4694c1d089b1370bc5
BrianK66618 <kbrian0618@gmail.com>
2024-01-19 08:27:59 -0500

    Colors and sounds in a square

--- Sandbox.js
+++ Sandbox.js
@@ -12,7 +12,7 @@ async function startProgram() {
 	setSpeed(0);
 	
 }
-*/
+
 
 // Square w/ different colors for each line
 //Date:1.18.2024
@@ -26,19 +26,35 @@ async function startProgram() {
 		await roll(270, 50, 2)	
 		setMainLed({r:54,g:190,b:227})
 		await roll(360, 50, 2)	
-		
+	
+
+	}
+}
+*/
+// Square w/ different colors & Sound for each line
+
+
+async function startProgram() {
+	for (var _i1 = 0; _i1 < 8; _i1++) {
+		await speak('aa aa aa aa aa aa aa aa aa aa',true)
+		setMainLed({r:209,g:19,b:19})
+		await roll(90, 45, 3)
+		await speak('ooga chaka ooga chaka', true)
+		setMainLed({r:255,g:205,b:3})
+		await roll(180, 45, 3)	
+		await Sound.play(true)
+		setMainLed({r:78,g:255,b:3})
+		await roll(270, 45, 3)	
+		await Sound.Animal.play(true)
+		setMainLed({r:54,g:190,b:227})
+		await roll(360, 45, 3)	
 	
 
 	}
 }
 
-// Square w/ different colors & Sound for each line
+
 /*
-
-
-
-
-
 // Figure 8
 //Date:1.18.2024
 async function startProgram() {
