21d5c135bbf704e60ddd7d1e7b790de601f0e34e
BrianK66618 <kbrian0618@gmail.com>
2024-01-22 08:36:42 -0500

    Initial Start Message

--- Maze.js
+++ Maze.js
@@ -1,5 +1,11 @@
 // Team Members: Brian Kuhn & Jensen Muday
+// Name: Brian Kuhn
 // Date: 1.22.2024
 // Program: Maze
 // Bolt-ID: SB-F28B
 
+//Intial Start Message
+async function startProgram() {
+	await scrollMatrixText('Start!', {r:9, g:255, b:123}, 25, true)
+	await speak('Start')
+}
