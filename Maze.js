f9ac0170161a99bfe314658aa98f57da3d0866ba
BrianK66618 <kbrian0618@gmail.com>
2024-01-25 07:59:04 -0500

    Red!

--- Maze.js
+++ Maze.js
@@ -16,6 +16,14 @@ async function startProgram() {
 	setMainLed({r:1, g:1, b:253})
 	await scrollMatrixText('blue', {r:2, g:2, b:123}, 25, true)
 	await speak('blue')
+	//sound 1
 	await roll(90, 52.5, 2)
 	await Sound.play(true)
+	//roll to red, around corner then red LED
+	await roll(180,68,1)
+	await roll(225, 68, 1)
+	setMainLed({r:224, g:1, b:0})
+	
+	//reset
+	await roll(0,0,2)
 }
